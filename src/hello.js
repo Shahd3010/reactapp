dba567de6d4166a92e0a3d8c23a8905658dc7516
Shahd3010 <73404827+Shahd3010@users.noreply.github.com>
2022-10-10 19:38:46 +0300

    new

---
+++ src/hello.js
@@ -0,0 +1,16 @@
+import React , {Component} from "react";
+import "./hello.css";
+
+
+class Hello extends Component{
+    render(){
+        return (
+        <div>
+
+            <h1> hello world </h1>
+            <p> Welcome to React</p>
+        </div> );
+       
+    }
+} 
+export default Hello;
